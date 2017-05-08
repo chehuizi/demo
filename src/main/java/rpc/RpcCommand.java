18f94c1a2cc552d16972d7e639d6048baf3f36cc
chehuizi <840739100@qq.com>
2017-05-08 08:23:59 +0800

    update demo

--- src/main/java/rpc/RpcCommand.java
+++ src/main/java/rpc/RpcCommand.java
@@ -3,44 +3,45 @@ package rpc;
 import java.io.Serializable;
 
 public class RpcCommand implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	String className ;
-    String methodName ;
-    String[] argumetsType ;
 
-    Object[] params ;
-    public String getClassName() {
-        return className;
-    }
+	String className;
+	String methodName;
+	String[] argumetsType;
 
-    public void setClassName(String className) {
-        this.className = className;
-    }
+	Object[] params;
 
-    public String getMethodName() {
-        return methodName;
-    }
+	public String getClassName() {
+		return className;
+	}
 
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
+	public void setClassName(String className) {
+		this.className = className;
+	}
 
-    public String[] getArgumetsType() {
-        return argumetsType;
-    }
+	public String getMethodName() {
+		return methodName;
+	}
 
-    public void setArgumetsType(String[] argumetsType) {
-        this.argumetsType = argumetsType;
-    }
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
 
-    public Object[] getParams() {
-        return params;
-    }
+	public String[] getArgumetsType() {
+		return argumetsType;
+	}
 
-    public void setParams(Object[] params) {
-        this.params = params;
-    }
+	public void setArgumetsType(String[] argumetsType) {
+		this.argumetsType = argumetsType;
+	}
+
+	public Object[] getParams() {
+		return params;
+	}
+
+	public void setParams(Object[] params) {
+		this.params = params;
+	}
 
 }
