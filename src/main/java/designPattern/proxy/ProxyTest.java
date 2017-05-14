55c2888bf531dae211465e738472ef33d7b5e7c9
chehuizi <840739100@qq.com>
2017-05-14 10:29:52 +0800

    update demo

--- src/main/java/designPattern/proxy/ProxyTest.java
+++ src/main/java/designPattern/proxy/ProxyTest.java
@@ -5,11 +5,8 @@ public class ProxyTest {
 	public static void main(String args[]) {
 		try {
 			DogHandler dogHandler = new DogHandler();
-			Dog dog = new Dog();
-			dogHandler.bind(dog);
-			dogHandler.invoke(dogHandler, Action.class.getMethod("speak", null), args);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
+			Action proxy = (Action) dogHandler.bind(new Dog());
+			proxy.speak();
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (Throwable e) {
