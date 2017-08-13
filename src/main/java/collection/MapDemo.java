527c260e68b92757d7b3cae3077b6a97cbb952ef
chehuizi <840739100@qq.com>
2017-08-13 10:08:40 +0800

    update files

--- src/main/java/collection/MapDemo.java
+++ src/main/java/collection/MapDemo.java
@@ -1,7 +1,9 @@
 package collection;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,8 +12,10 @@ public class MapDemo {
 	public static void main(String args[]) {
 		Map<String, String> map = new HashMap<String, String>();
 		Map<String, String> conMap = new ConcurrentHashMap<String, String>();
+		Map<String, String> linkedMap = new LinkedHashMap<String, String>(100, 0.75f, true);
 //		run(map);
-		run(conMap);
+//		run(conMap);
+		entry(linkedMap);
 	}
 	
 	public static void run(Map<String, String> map) {
@@ -20,5 +24,21 @@ public class MapDemo {
 		MapDemoRunnable runnable2 = new MapDemoRunnable(map, "id", "2");
 		executor.execute(runnable1);
 		executor.execute(runnable2);
+	}
+	
+	public static void entry(Map<String, String> map) {
+		for (int i=1; i<20; i++) {
+			map.put(i + "", i + "");
+		}
+		for (Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry);
+		}
+		map.get("8");
+		map.get("9");
+		map.get("1");
+		System.out.println("----------------------");
+		for (Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry);
+		}
 	}
 }
