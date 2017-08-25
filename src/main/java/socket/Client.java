0ab2aa77fed1d29c0870557d5aa44e824bc83898
chehuizi <840739100@qq.com>
2017-08-25 12:40:02 +0800

    add socket files

---
+++ src/main/java/socket/Client.java
@@ -0,0 +1,65 @@
+package socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class Client {
+	public void connect() {
+		try {
+			Socket client = new Socket("127.0.0.1", 9999);
+			handle(client);
+			Socket client1 = new Socket("127.0.0.1", 9999);
+			handle(client1);
+		} catch (IOException ioe) {
+			System.out.println("connect error...");
+		}
+	}
+	
+	void handle(Socket socket) {
+		Thread handler = new Thread(new Handler(socket));
+		handler.start();
+	}
+	
+	class Handler implements Runnable {
+		Socket socket;
+		
+		public Handler(Socket socket) {
+			this.socket = socket;
+			
+		}
+		
+		public void run() {
+			try {
+				int index = 0;
+				byte[] byteData = new byte[1024];
+				int next;
+				do {
+					InputStream ins = socket.getInputStream();
+					next = ins.read();
+					byteData[index] = (byte) next;
+					index++;
+					String str = new String(byteData);
+					System.out.println(Thread.currentThread().getName() + str);
+				} while (next > 0);
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+			
+		}
+
+		public Socket getSocket() {
+			return socket;
+		}
+
+		public void setSocket(Socket socket) {
+			this.socket = socket;
+		}
+		
+	}
+	
+	public static void main(String args[]) {
+		Client client = new Client();
+		client.connect();
+	}
+}
---
+++ src/main/java/socket/Server.java
@@ -0,0 +1,72 @@
+package socket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	public void accept() {
+		try {
+			ServerSocket ss = new ServerSocket(9999);
+			
+			while (true) {
+				Socket client = ss.accept();
+				if (client != null) {
+					System.out.println("get a client...");
+					handle(client);
+				}
+			}
+			
+		} catch (IOException ioe) {
+			System.out.println("accept error...");
+		}
+	}
+	
+	private void handle(Socket client) {
+		if (client != null) {
+			Thread handler = new Thread(new Handler(client));
+			handler.start();
+		}
+	}
+	
+	class Handler implements Runnable {
+		Socket socket;
+		
+		public Handler(Socket socket) {
+			this.socket = socket;
+		}
+		
+		public void run() {
+			while (true) {
+				String str = "hello world";
+				try {
+					OutputStream os = socket.getOutputStream();
+					os.write(str.getBytes());
+					os.flush();
+					
+					Thread.sleep(5000);
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				} catch (InterruptedException ine) {
+					ine.printStackTrace();
+				}
+			}
+		}
+
+		public Socket getSocket() {
+			return socket;
+		}
+
+		public void setSocket(Socket socket) {
+			this.socket = socket;
+		}
+
+	}
+	
+	public static void main(String args[]) {
+		Server server = new Server();
+		server.accept();
+	}
+}
