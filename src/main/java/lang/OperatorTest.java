544c5a96dec7ac54583026ef5952696f39e75c87
chehuizi <840739100@qq.com>
2017-09-02 20:05:51 +0800

    update operator test

--- src/main/java/lang/OperatorTest.java
+++ src/main/java/lang/OperatorTest.java
@@ -4,10 +4,16 @@ public class OperatorTest {
 	
 	public static void shift() {
 		int i = -1 << 29;
-		System.out.println(i);
-		int j = (1 << 29) - 1;
-		System.out.println(j);
-		System.out.println(i & j);
+		System.out.println("running : " + i);
+		int ctl = i | 0;
+		System.out.println("ctl : " + i);
+		int capacity = (1 << 29) - 1;
+		System.out.println("capacity : " + capacity);
+		int wc = i & capacity;
+		System.out.println("wc : " + wc);
+		int rs = ctl & ~capacity;
+		System.out.println("rs : " + rs);
+		
 	}
 	
 	public static void main(String args[]) {
